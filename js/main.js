72413955979e1c0e90b164d15b51a9f12fe373bc
Maxime Parmentier <parmentier.maxime@gmail.com>
2014-11-27 09:13:42 -0800

    fix instagram feed

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 $(function () {
 
-    // Instafeed
-    var feed = new Instafeed({
-        get: 'user',
-        userId: 461656,
-        sortBy: 'most-recent',
-        limit: 20,
-        clientId: 'a00a720282c4443081e55c2aba3af83f',
-        accessToken: '461656.a00a720.e2877a3aea6a4be1b11eedc09932ec9b',
-        template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
-        filter: function (data) {
-            return data.caption.text.indexOf('7:36pm') >= 0;
-        }
-    });
-    feed.run();
-
-
     // Runkeeper Feed
     $.get('http://services.iamnerdwin.com/runkeeper/feed/v2/1162026').done(function (rss) {
         ul = $('.runkeeper-list');
         $(rss).find("item").each(function (i, el) {
             if (i > 7) return;
             ul.append('<li><a target="_blank" href="' + $('link', el).text() + '">' + $('title', el).text() + '</a></li>');
+        });
+    });
+
+
+    // Instagram Feed
+    $.ajax({
+        url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=http://iconosquare.com/feed/maxparm',
+        dataType: 'jsonp'
+    }).done(function (r) {
+        ul = $('.instafeed-list');
+        var i = 0;
+        _.each(r.responseData.feed.entries, function (e) {
+            if (e.title.indexOf('7:36pm') != 0) return;
+            i+=1;
+            if (i > 7) return;
+            src = $('img', e.content).attr('src')
+            ul.append('<li><a target="_blank" href="' + e.link + '"><img src="' + src + '"/></a></li>');
         });
     });
 
